f0f206f13437f4e2a6c01e73ae020a29a9500566
RomanCypherpunk <enzo.xs@hotmail.com>
2025-08-14 14:07:13 -0300

    Calculadora de IMC

---
+++ modulo_1_logica_de_programacao/aula12_calculadora_IMC.py
@@ -0,0 +1,11 @@
+nome = "Enzo Xavier"
+idade = 20
+altura_em_metros = 1.75
+peso_em_kg = 74.5
+imc = round(peso_em_kg / (altura_em_metros ** 2),1)
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura (m):", altura_em_metros)
+print("Peso (kg):", peso_em_kg)
+print("IMC: ", imc)
