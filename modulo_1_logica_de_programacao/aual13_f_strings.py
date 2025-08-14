314f688ea1bac797bc89dd02e91ee16a7e873c15
RomanCypherpunk <enzo.xs@hotmail.com>
2025-08-14 14:12:21 -0300

    Como usar F strings

---
+++ modulo_1_logica_de_programacao/aual13_f_strings.py
@@ -0,0 +1,14 @@
+nome = 'Enzo Xavier'
+altura = 1.75
+peso = 74.5
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
