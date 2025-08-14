ff3d73f1039e6c7ac345ee28d4bf7784e85094c3
RomanCypherpunk <enzo.xs@hotmail.com>
2025-08-14 14:26:23 -0300

    Documentando aula 13

--- modulo_1_logica_de_programacao/aula_013_f_strings.py
+++ modulo_1_logica_de_programacao/aula_013_f_strings.py
@@ -1,14 +1,26 @@
+# Definindo as variáveis
 nome = 'Enzo Xavier'
 altura = 1.75
 peso = 74.5
 imc = peso / altura ** 2
 
-"f-strings"
-linha_1 = f'{nome} tem {altura:.2f} de altura,'
-linha_2 = f'pesa {peso} quilos e seu imc é'
-linha_3 = f'{imc:.2f}'
+# Usando f-strings para formatar strings de forma simples e legível
+# Basta colocar um 'f' antes das aspas e usar chaves {} para inserir variáveis ou expressões
 
-print(linha_1)
-print(linha_2)
-print(linha_3)
+# Exemplo básico:
+print(f'O nome é {nome}')
+
+# Também é possível formatar números, como limitar casas decimais:
+print(f'{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}')
+
+# Explicando a formatação:
+# {altura:.2f} -> mostra a altura com 2 casas decimais
+# {imc:.2f}    -> mostra o IMC com 2 casas decimais
+
+# Você pode criar várias linhas usando f-strings:
+mensagem = (
+    f'{nome} tem {altura:.2f} de altura,\n'
+    f'pesa {peso} quilos e seu IMC é {imc:.2f}'
+)
+print(mensagem)
 
