efa5dc5e1a0e32cc78102ab122a351e6d32d8d85
Enzo Xavier Santos <92555923+RomanCypherpunk@users.noreply.github.com>
2025-10-08 19:56:02 +0000

    CodeWars: Credit Card

---
+++ CodeWars/CreditCard/best_pratice.py
@@ -0,0 +1,8 @@
+def maskify(cc):
+    mascara = "#"*(len(cc)-4) + cc[-4:]
+    print(f"O número do seu cartão é: {mascara}")
+    return mascara
+
+
+cartao = input("Digite o número do seu cartão: ")
+maskify(cartao)
