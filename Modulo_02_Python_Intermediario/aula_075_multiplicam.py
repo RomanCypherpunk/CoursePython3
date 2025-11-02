e9441e9b9043d4cc50506b0b7dbbfd48a5926321
Enzo Xavier <seu_email@exemplo.com>
2025-11-02 12:14:11 -0300

    Aula 75

---
+++ Modulo_02_Python_Intermediario/aula_075_multiplicam.py
@@ -0,0 +1,14 @@
+def duplicar(numero: int):
+    return numero * 2
+
+def triplicar(numero: int):
+    return numero * 3
+
+def quadruplicar(numero: int):
+    return numero * 4
+
+entrada: int = int(input("Digite um número: "))
+
+print(duplicar(entrada))
+print(triplicar(entrada))
+print(quadruplicar(entrada))
