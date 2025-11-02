74e5a42aa0fcf1abd2c7ff442df09facb269f767
Enzo Xavier <seu_email@exemplo.com>
2025-11-02 12:14:06 -0300

    Aula 74

--- Modulo_02_Python_Intermediario/aula_072_funcoes.py
+++ Modulo_02_Python_Intermediario/aula_072_funcoes.py
@@ -1,11 +1,11 @@
-def multiplicador(*numeros):
-    resultado = 1
+def multiplicador(*numeros: int):
+    resultado: int = 1
     for n in numeros:
         resultado *= n
     return resultado
 
-def parOuImpar(numero):
-    return "Par" if numero % 2 == 0 else "Ímpar"
+def parOuImpar(numero: float):
+    return f"{numero} é par" if numero % 2 == 0 else f"{numero} é ímpar"
 
 print(multiplicador(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 print(parOuImpar(999))
