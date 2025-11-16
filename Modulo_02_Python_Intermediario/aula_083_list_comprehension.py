2c631f5e2f2408d841b57260ed9e9eada9e7cd46
Enzo Xavier <seu_email@exemplo.com>
2025-11-16 11:35:44 -0300

    Aula 83

---
+++ Modulo_02_Python_Intermediario/aula_083_list_comprehension.py
@@ -0,0 +1,8 @@
+lista1 = [numero for numero in range(10)]
+print(lista1)
+
+lista2 = [numero * numero for numero in range(10)]
+print(lista2)
+
+lista3 = [numero * "a" for numero in range(10)]
+print(lista3)
